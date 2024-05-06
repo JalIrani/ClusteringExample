cab4c35ca05919c27c05b1abf842f6ae3b535759
Jal Irani <jaljasonirani@gmail.com>
2024-05-06 18:45:27 -0400

    init commit with clustering

---
+++ ClusteringExample/ClusteringExampleApp.swift
@@ -0,0 +1,18 @@
+//
+//  ClusteringExampleApp.swift
+//  ClusteringExample
+//
+//  Created by Irani, Jal J. on 5/3/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ClusteringExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClusteringExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ClusteringExample
+//
+//  Created by Irani, Jal J. on 5/3/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+
+    var body: some View {
+        MapView()
+            .edgesIgnoringSafeArea(.all)
+    }
+}
+
+#Preview {
+    ContentView()
+}
